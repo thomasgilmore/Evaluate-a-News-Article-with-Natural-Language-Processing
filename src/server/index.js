aa260912d4676c34565a9ec31e65b0c59e8d7de0
Thomas Gilmore <tomtitans@aol.com>
2019-11-14 18:53:36 -0500

    docs: Commit Project Files

---
+++ src/server/index.js
@@ -0,0 +1,31 @@
+const dotenv = require('dotenv');
+dotenv.config();
+var path = require('path')
+const express = require('express')
+const mockAPIResponse = require('./mockAPI.js')
+
+var aylien = require("aylien_textapi");
+
+const app = express()
+
+var textapi = new aylien({
+    application_id: process.env.API_ID,
+    application_key: process.env.API_KEY
+    });
+
+app.use(express.static('dist'))
+
+console.log(__dirname)
+
+app.get('/', function (req, res) {
+    res.sendFile('dist/index.html')
+})
+
+// designates what port the app will listen to for incoming requests
+app.listen(8080, function () {
+    console.log('Example app listening on port 8080!')
+})
+
+app.get('/test', function (req, res) {
+    res.send(mockAPIResponse)
+})
